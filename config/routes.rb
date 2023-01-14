35bd798faa04a0cf36e9305326ff1cce916855d8
Axel Regairaz <axel.regairaz@qobuz.com>
2023-01-14 17:04:48 +0100

    add booking model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :pokemons, only: %i[index show new create]
+  resources :bookings, only: %i[index show new create]
 end
