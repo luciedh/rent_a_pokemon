9568e53d4702abf4b3f98a43e435ba648a5008af
Clement Perrin <113604057+ClemPerr1@users.noreply.github.com>
2023-01-14 17:25:22 +0100

    Add User model and Auth/Registration logic

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, :username, :email, :password, presence: true
+  validates :username, :email, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  has_many :pokemons, :bookings
+end
