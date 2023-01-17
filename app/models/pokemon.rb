c0e0b22d7ccb34cfabe0f55a712ca6f3f91dd0f6
Axel Regairaz <axel.regairaz@qobuz.com>
2023-01-17 21:14:07 +0100

    Day 2

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,2 +1,6 @@
 class Pokemon < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
---
+++ db/migrate/20230117191217_add_user_references_to_pokemons.rb
@@ -0,0 +1,5 @@
+class AddUserReferencesToPokemons < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :pokemons, :user, null: false, foreign_key: true
+  end
+end
