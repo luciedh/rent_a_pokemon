e0a183fd4cf5f982bc083541367386802c6f4502
Beata Gyorgy <gybeacska19@gmail.com>
2023-01-21 16:39:25 +0100

    card layout, add booking new to pokemon show

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -4,13 +4,14 @@ class PokemonsController < ApplicationController
       @pokemons = Pokemon.where("name ILIKE ?", "%#{params[:query]}%")
       @pokemons = Pokemon.where.not(user: nil) if @pokemons.empty?
     else
-      # @pokemons = Pokemon.where.not(user: nil)
-      @pokemons = Pokemon.all
+      @pokemons = Pokemon.where.not(user: nil)
+      # @pokemons = Pokemon.all
     end
   end
 
   def show
     @pokemon = Pokemon.find(params[:id])
+    @booking = Booking.new
   end
 
   def edit
