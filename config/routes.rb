4fe52ccd5b7642fc1288b592ad2b3fb534ebf146
Clement Perrin <113604057+ClemPerr1@users.noreply.github.com>
2023-01-21 15:23:39 +0100

    User can compose his team

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  resources :pokemons, only: %i[index show] do
+  resources :users, only: [:show, :edit, :update]
+  resources :pokemons, only: %i[index show update] do
     resources :bookings, only: %i[new create]
   end
   resources :bookings, only: %i[index show edit update destroy]
+  get 'users/:id/team', to: 'pokemons#edit', as: "team"
 end
