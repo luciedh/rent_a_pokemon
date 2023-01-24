b682f63b244fc0abf09b5a5e1df0f752eb80d246
Axel Regairaz <axel.regairaz@qobuz.com>
2023-01-24 21:55:45 +0100

    PokemonsInfos2

---
+++ db/migrate/20230124201540_remove_hours_to_dates.rb
@@ -0,0 +1,5 @@
+class RemoveHoursToDates < ActiveRecord::Migration[7.0]
+  def change
+    change_column :bookings, :start_date, :date
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_24_185440) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_24_201540) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,7 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_24_185440) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.bigint "pokemon_id", null: false
-    t.datetime "start_date"
+    t.date "start_date"
     t.index ["pokemon_id"], name: "index_bookings_on_pokemon_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
