5049c4b744011c206317023ef470ca342ffec05c
Beata Gyorgy <gybeacska19@gmail.com>
2023-01-26 18:25:43 +0100

    checkbox style and update team correction

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,8 +1,7 @@
 class PokemonsController < ApplicationController
   def index
     if params[:query].present?
-      sql_query = "name ILIKE :query OR type1 ILIKE :query OR type2 ILIKE :query"
-      @pokemons = Pokemon.where(sql_query, query: "%#{params[:query]}%")
+      @pokemons = Pokemon.where("name ILIKE ?", "%#{params[:query]}%")
       @pokemons = Pokemon.where.not(user: nil) if @pokemons.empty?
     else
       @pokemons = Pokemon.where.not(user: nil)
@@ -21,11 +20,20 @@ class PokemonsController < ApplicationController
   end
 
   def update
+    pokemons_all = Pokemon.all.find_all { |h| h[:user_id] == params[:id].to_i }
+    pokemons_existing = pokemons_all.map { |h| h[:id].to_s }
     pokemons_selected = params[:user][:pokemon_ids]
     pokemons_selected.shift
-    pokemons_selected.each do | pokemon_id |
+    pokemons_uncheck = pokemons_existing - pokemons_selected
+    pokemons_check  = pokemons_selected - pokemons_existing
+    pokemons_check.each do |pokemon_id|
       pokemon = Pokemon.find(pokemon_id)
       pokemon[:user_id] = params[:id]
+      pokemon.save
+    end
+    pokemons_uncheck.each do |pokemon_id|
+      pokemon = Pokemon.find(pokemon_id)
+      pokemon[:user_id] = nil
       pokemon.save
     end
     redirect_to user_path(current_user.id)
